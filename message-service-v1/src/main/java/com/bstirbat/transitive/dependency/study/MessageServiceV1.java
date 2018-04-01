9a2d98f8ea5d5e74ef2d144041848560a8fb0e94
Bogdan Stirbat <bogdan.stirbat@gmail.com>
2018-04-01 15:21:05 +0300

    added message service v2; will use message library version 2

--- message-service-v1/src/main/java/com/bstirbat/transitive/dependency/study/MessageServiceV1.java
+++ message-service-v1/src/main/java/com/bstirbat/transitive/dependency/study/MessageServiceV1.java
@@ -1,8 +1,8 @@
 package com.bstirbat.transitive.dependency.study;
 
 
-public class MessageServiceV1
-{
+public class MessageServiceV1 {
+
     public static String getMessage() {
         return MessageLibrary.getMessage();
     }
--- message-service-v2/src/main/java/com/bstirbat/transitive/dependency/study/MessageServiceV2.java
+++ message-service-v2/src/main/java/com/bstirbat/transitive/dependency/study/MessageServiceV2.java
@@ -1,9 +1,9 @@
 package com.bstirbat.transitive.dependency.study;
 
 
-public class MessageLibrary {
+public class MessageServiceV2 {
 
     public static String getMessage() {
-        return "message-version-1";
+        return MessageLibrary.getMessage();
     }
 }
