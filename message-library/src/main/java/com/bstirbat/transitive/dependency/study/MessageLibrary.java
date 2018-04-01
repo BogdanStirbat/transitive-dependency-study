d845839a2bbc00a076048a02432c8b369f31d83d
Bogdan Stirbat <bogdan.stirbat@gmail.com>
2018-04-01 15:02:44 +0300

    added the message library, version 1.0-SNAPSHOT

---
+++ message-library/src/main/java/com/bstirbat/transitive/dependency/study/MessageLibrary.java
@@ -0,0 +1,12 @@
+package com.bstirbat.transitive.dependency.study;
+
+/**
+ * Hello world!
+ *
+ */
+public class MessageLibrary
+{
+    public static String getMessage() {
+        return "message-version-1";
+    }
+}
